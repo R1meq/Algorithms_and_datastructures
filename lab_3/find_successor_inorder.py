f57ab6cfba2c8132beedada53573e5a1f9fa0fe4
R1meq <115041231+R1meq@users.noreply.github.com>
2023-10-20 08:44:44 +0300

    add lab_3

---
+++ lab_3/find_successor_inorder.py
@@ -0,0 +1,32 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+
+def find_successor(tree: BinaryTree, node: BinaryTree) -> BinaryTree:
+
+    if node.parent is None:
+        return None
+
+    if node.right:
+        current = node.right
+        while current.left:
+            current = current.left
+        return current
+
+    if node.value < tree.value and node.value < node.parent.value:
+        return node.parent
+
+    if node.value < tree.value and node.value > node.parent.value:
+        current = node.parent
+        while current.right == node and node is not None:
+            node = current
+            current = current.parent
+        return current
+
+    if tree.value < node.value and node.parent.value > node.value:
+        return node.parent
